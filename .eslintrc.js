ddae14da41580089b6ba50d039ff9d463b8d053c
Mary Volosevich <mary.valasevich@gmail.com>
2022-12-18 12:05:18 +0300

    Постаралась исправить ошибки в esLint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,30 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: [
-    'plugin:react/recommended',
-    'airbnb',
-  ],
-  overrides: [
-  ],
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-  },
-  plugins: [
-    'react',
-  ],
-  rules: {
-  },
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: [
+        'plugin:react/recommended',
+        'airbnb',
+    ],
+    overrides: [
+    ],
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+    },
+    plugins: [
+        'react',
+    ],
+    rules: {
+        'jsx-a11y/no-static-element-interactions': 'off',
+        'jsx-a11y/click-events-have-key-events': 'off',
+        'react/react-in-jsx-scope': 'error',
+        'no-trailing-spaces': 'error',
+        'import/no-duplicates': 'off',
+        'quote-props': 'off',
+        'indent': ['error', 4],
+        'react/jsx-indent': ['error', 4],
+        'react/jsx-indent-props': ['error', 4],
+    },
 };
